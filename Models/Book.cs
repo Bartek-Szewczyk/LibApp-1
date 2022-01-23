f4e0462f395b5b4f57d66a38803029e5bd354f3b
Bartek-Szewczyk <ba20001000@gmail.com>
2022-01-23 10:58:53 +0100

    added validation for BookForm

--- Models/Book.cs
+++ Models/Book.cs
@@ -11,16 +11,21 @@ namespace LibApp.Models
         public int Id { get; set; }
 		[Required]
 		[StringLength(255)]
+		[Display(Name="Book Name")]
 		public string Name { get; set; }
 		[Required]
+		[Display(Name="Author Name")]
 		public string AuthorName { get; set; }
 		[Required]
 		public Genre Genre { get; set; }
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
 		[Display(Name="Release Date")]
+		[Required]
 		public DateTime ReleaseDate { get; set; }
 		[Display(Name="Number in Stock")]
+		[Required]
+		[Range(1,20, ErrorMessage = "Range between 1 and 20")]
 		public int NumberInStock { get; set; }
 		public int NumberAvailable { get; set; }
 	}
