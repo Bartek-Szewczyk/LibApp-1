5002341e42c8348865730014b0a7de992a848b89
Bartek-Szewczyk <ba20001000@gmail.com>
2022-02-05 14:13:18 +0100

    added roles

--- Startup.cs
+++ Startup.cs
@@ -40,12 +40,14 @@ namespace LibApp
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
         {
             if (env.IsDevelopment())
             {
@@ -73,6 +75,48 @@ namespace LibApp
                     pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+            CreateRoles(serviceProvider).Wait();
+        }
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles 
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+            string[] roleNames = { "Owner", "StoreManager", "User" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+
+            //Here you could create a super user who will maintain the web app
+            foreach (var userRole in roleNames)
+            {
+                var user = new IdentityUser();
+                user.Email = userRole +"@mail.com";
+                user.UserName = userRole + "@mail.com";
+                user.EmailConfirmed = true;
+                            
+                            string userPWD = "Password@1234";
+                            
+                            var _user = await UserManager.FindByEmailAsync(userRole + "@mail.com");
+                
+                            if(_user == null)
+                            {
+                                var createPowerUser = await UserManager.CreateAsync(user, userPWD);
+                                if (createPowerUser.Succeeded)
+                                    await UserManager.AddToRoleAsync(user, userRole);
+                                    
+                            }
+            }
+            
+            
             
         }
     }
